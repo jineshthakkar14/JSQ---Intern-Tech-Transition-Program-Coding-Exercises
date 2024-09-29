54664446809aeec4d7648c349bd4ab90caab627c
jineshthakkar14 <jinesh.22110264@viit.ac.in>
2024-09-29 16:18:15 +0530

    Updated PasswordGenerator.jsx
    Added the feature of copy to clipboard, the copied msg will there on UI until 2sec after copying

--- React/React Password Generator/src/components/PasswordGenerator.jsx
+++ React/React Password Generator/src/components/PasswordGenerator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaClipboard } from 'react-icons/fa';
 
 const PasswordGenerator = () => {
   const [password, setPassword] = useState('');
@@ -7,6 +8,7 @@ const PasswordGenerator = () => {
   const [includeLowercase, setIncludeLowercase] = useState(false);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [includeSymbols, setIncludeSymbols] = useState(false);
+  const [copyMessage, setCopyMessage] = useState('');
 
   //Generate Password logic
   const generatePassword = () => {
@@ -34,15 +36,29 @@ const PasswordGenerator = () => {
     setPassword(newPassword);
   };
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(password);
+    setCopyMessage('Copied!'); // Set the copy message
+    setTimeout(() => setCopyMessage(''), 2000); // Clear the message after 2 seconds
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h1 className="text-2xl font-bold mb-6 text-center">Password Generator</h1>
 
         {/* Password Display */}
-        <div className="bg-gray-200 p-3 mb-6 rounded-md text-center text-lg font-mono">
-          {password || "Your password will appear here"}
+        <div className="flex items-center justify-between bg-gray-200 p-3 mb-2 rounded-md text-lg font-mono">
+            <span className="mr-2">{password || 'Your password will appear here'}</span>
+            <FaClipboard
+                className="h-6 w-6 cursor-pointer text-gray-600 hover:text-gray-800"
+                onClick={copyToClipboard}
+            />
         </div>
+
+
+        {/* Copy Message Display */}
+        {copyMessage && <div className="text-green-500 text-center mb-2">{copyMessage}</div>}
 
         {/*Password Length slider*/}
         <div className="mb-4">
