4962451d5a8f113c3866f1d3767ecc283aa39279
jineshthakkar14 <jinesh.22110264@viit.ac.in>
2024-10-02 00:51:37 +0530

    Created get_product_by_id api
    /products/{id} will get product by id

--- SQL/app/controllers/productControllers.py
+++ SQL/app/controllers/productControllers.py
@@ -4,3 +4,7 @@ from app.models.product import Product
 # Get all products
 def get_all_products(db: Session):
     return db.query(Product).all()
+
+# Get a product by its ID
+def get_product_by_id(product_id: int, db: Session):
+    return db.query(Product).filter(Product.id == product_id).first()
--- SQL/app/routes/productRoutes.py
+++ SQL/app/routes/productRoutes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.config.db import get_db
 from app.schemas.product import ProductResponse
-from app.controllers.productControllers import get_all_products
+from app.controllers.productControllers import get_all_products,get_product_by_id
 
 router = APIRouter()
 
@@ -10,3 +10,11 @@ router = APIRouter()
 @router.get("/products", response_model=list[ProductResponse])
 def get_products(db: Session = Depends(get_db)):
     return get_all_products(db)
+
+# Get a product by ID
+@router.get("/products/{product_id}", response_model=ProductResponse)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = get_product_by_id(product_id, db)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
